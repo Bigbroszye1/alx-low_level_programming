a6c4c124248bc4c27809054a4a48acefa29204f8
Thomas Bulus <thomas.bigbros14@gmail.comgit config --global user.name Bigbroszye1>
2022-12-10 01:19:54 -0800

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * Main - Entry point
+ * Main: Entry point for the program
  *
  * Return: Always 0 (success)
  *
@@ -8,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
