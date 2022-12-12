29c20ef3c025baba75e1e02c17a3d7c20dfaa975
Bigbroszye1 <thomas.bigbroszye14@gmail.com>
2022-12-11 16:44:23 -0800

    variable if else advanced tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,20 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			putchar((digit3 % 10) + '0');
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-			if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-				continue;
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
 
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
