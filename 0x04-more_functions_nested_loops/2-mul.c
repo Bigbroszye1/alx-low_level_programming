ee1c5cfb61428cbcdb3b27301a2c76b57fbab332
Bigbroszye1 <thomas.bigbroszye14@gmail.com>
2022-12-15 11:19:45 -0800

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul - multiplies two numbers
+ *@a: the first integer
+ *@b: the second integer
+ *Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - printing line
+ * @n: number of time
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
